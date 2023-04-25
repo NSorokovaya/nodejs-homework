69bca6a4a33288b96a08e9faaa3a47a085d106de
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-04-25 12:24:49 -0400

    c

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,8 @@
 const addContactsSchema = require("./addContactSchema");
 const putContactsSchema = require("./putContactsSchema");
-
+const updateFavorite = require("./updateFavorite");
 module.exports = {
   addContactsSchema,
   putContactsSchema,
+  updateFavorite,
 };
