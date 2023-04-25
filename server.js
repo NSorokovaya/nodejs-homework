ce871777346b74138ab79a163bfb2d7d29bf5f5c
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-04-25 16:39:13 -0400

    commit

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { DB_HOST } = process.env;
-// const DB_HOST =
-//   "mongodb+srv://nadiaasorokovaya:ghost2806far@nsorokovaya.h2i1pmt.mongodb.net/test";
+
 mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
