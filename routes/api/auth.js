fe127db3116dcc7d119ec51ca7864eb3562f507a
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-04-27 13:00:54 -0400

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas");
+const ctrl = require("../../controllers/auth/register");
+
 const router = express.Router();
 
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 module.exports = router;
--- schemas/index.js
+++ schemas/index.js
@@ -1,8 +1,10 @@
 const addContactsSchema = require("./addContactSchema");
 const putContactsSchema = require("./putContactsSchema");
 const updateFavorite = require("./updateFavorite");
+const registerSchema = require("./registerSchema");
 module.exports = {
   addContactsSchema,
   putContactsSchema,
   updateFavorite,
+  registerSchema,
 };
